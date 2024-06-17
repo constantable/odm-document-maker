854c6b54ca2c97bf7fbe88b2a67fa9c9ec015cf0
Constantable <itc.masters@gmail.com>
2024-06-18 00:18:41 +0400

    Removed unused code. Copyright messages

--- src/Util/ClassSourceManipulator.php
+++ src/Util/ClassSourceManipulator.php
@@ -1,5 +1,27 @@
 <?php
 
+/*
+ * Copyright (c) 2004-2020 Fabien Potencier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 namespace Constantable\OdmDocumentMaker\Util;
 
 use Constantable\OdmDocumentMaker\Doctrine\DoctrineODMHelper;
@@ -301,11 +323,6 @@ final class ClassSourceManipulator
         $this->addCollectionRelation($oneToMany);
     }
 
-    public function addManyToManyRelation(RelationManyToMany $manyToMany): void
-    {
-        $this->addCollectionRelation($manyToMany);
-    }
-
     public function addManyToManyReference(ReferenceManyToMany $manyToMany): void
     {
         $this->addCollectionRelation($manyToMany);
@@ -317,11 +334,6 @@ final class ClassSourceManipulator
 
         $this->getClassNode()->implements[] = new Node\Name(Str::getShortClassName($interfaceName));
         $this->updateSourceCodeFromNewStmts();
-    }
-
-    public function addAccessorMethod(string $propertyName, string $methodName, $returnType, bool $isReturnTypeNullable, array $commentLines = [], $typeCast = null): void
-    {
-        $this->addCustomGetter($propertyName, $methodName, $returnType, $isReturnTypeNullable, $commentLines, $typeCast);
     }
 
     public function addGetter(string $propertyName, $returnType, bool $isReturnTypeNullable, array $commentLines = []): void
@@ -399,16 +411,6 @@ final class ClassSourceManipulator
         }
 
         return $methodNodeBuilder;
-    }
-
-    public function createMethodLevelCommentNode(string $comment)
-    {
-        return $this->createSingleLineCommentNode($comment, self::CONTEXT_CLASS_METHOD);
-    }
-
-    public function createMethodLevelBlankLine()
-    {
-        return $this->createBlankLineNode(self::CONTEXT_CLASS_METHOD);
     }
 
     /**
@@ -1025,19 +1027,6 @@ final class ClassSourceManipulator
         $node = end($nodes);
 
         return false === $node ? null : $node;
-    }
-
-    /**
-     * @return Node[]
-     */
-    private function findAllNodes(callable $filterCallback): array
-    {
-        $traverser = new NodeTraverser();
-        $visitor = new NodeVisitor\FindingVisitor($filterCallback);
-        $traverser->addVisitor($visitor);
-        $traverser->traverse($this->newStmts);
-
-        return $visitor->getFoundNodes();
     }
 
     private function createBlankLineNode(string $context): Node\Stmt\Use_|Node|Node\Stmt\Property|Node\Expr\Variable
